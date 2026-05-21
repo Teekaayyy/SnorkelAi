b87444708d64d77886d1760df18866a230cad964
Teekaayyy <jessicatekome@gmail.com>
2026-05-21 12:16:10 +0100

    ggg

--- tests/test.sh
+++ tests/test.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
-
 mkdir -p /logs/verifier
 
-if [ "$PWD" = "/" ]; then
-    echo "Error: No working directory set."
-    echo 0 > /logs/verifier/reward.txt
-    exit 1
-fi
-
 cd /service
+pip install -q pytest==8.4.1 pytest-json-ctrf==0.3.5
 
-uvx \
-  -p 3.11 \
-  -w pytest==8.4.1 \
-  -w pytest-json-ctrf==0.3.5 \
-  pytest --ctrf /logs/verifier/ctrf.json /tests/test_outputs.py -rA
+python -m pytest --ctrf /logs/verifier/ctrf.json /tests/test_outputs.py -rA
 
 if [ $? -eq 0 ]; then
   echo 1 > /logs/verifier/reward.txt
